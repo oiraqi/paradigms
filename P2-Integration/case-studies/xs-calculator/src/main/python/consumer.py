9c275aa263dfa51976e0404a07d89e0254250ba4
Omar Iraqi Houssaini <38861318+oiraqi@users.noreply.github.com>
2024-05-25 04:26:02 +0100

    Update consumer.py

--- P2-Integration/case-studies/xs-calculator/src/main/python/consumer.py
+++ P2-Integration/case-studies/xs-calculator/src/main/python/consumer.py
@@ -1,9 +1,6 @@
 from zeep import Client
-
 calculator = Client('src/main//resources/CalculatorService.wsdl').service
-
 x, y = 7.0, 5.0
-
 result = calculator.add(x, y)
 print(str(x) + ' + ' + str(y) + ' = ' + str(result))
 
